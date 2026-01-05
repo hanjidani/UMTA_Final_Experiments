cb3524dfdd2cfba5c6fe79d30705f0bae490b986
hanjidani <hoseinanjidani2@gmail.com>
2026-01-05 16:13:06 +0330

    Fix: Export get_num_gpus from shared.utils

--- shared/utils/__init__.py
+++ shared/utils/__init__.py
@@ -1,4 +1,4 @@
-from .helpers import get_device, set_seed, clear_memory, count_parameters, ensure_dir
+from .helpers import get_device, get_num_gpus, set_seed, clear_memory, count_parameters, ensure_dir
 from .config import load_config, save_config
 
 
